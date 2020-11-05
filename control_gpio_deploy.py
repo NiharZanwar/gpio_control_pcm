4fbe1ec1b246b5814289a953c5fafb9631f329b3
Nihar zanwar <zanwarnihar@gmail.com>
2020-11-05 13:21:33 +0530

    Added trigger functionality for safe tower operation

--- control_gpio_deploy.py
+++ control_gpio_deploy.py
@@ -4,7 +4,7 @@ import json
 from time import sleep
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 key = {
     1: GPIO.HIGH,
@@ -38,6 +38,14 @@ def set_gpio(pin, value, inverted):
     GPIO.output(int(pin), key[abs(value - inverted)])
 
 
+def send_in_trigger(pin, config):
+    print("trigger to pin {}".format(pin))
+    GPIO.output(int(pin), key[1])
+    sleep(config['trigger_up_time_s'])
+    GPIO.output(int(pin), key[0])
+    sleep(config['trigger_down_time_s'])
+
+
 def handle_gpio(config, occupancy):
     if occupancy["occupancy"] >= occupancy["maximum_occupancy"]:
         for gpio_pin in config["gpio"]:
@@ -60,6 +68,9 @@ def get_occupancy_data(config):
 
 def main():
     config = initialize()
+    prev_in = None
+    prev_out = None
+    sleep(30)
     while True:
         try:
             initialize_gpio(config)
@@ -69,12 +80,35 @@ def main():
         sleep(5)
     while True:
         occupancy_data = get_occupancy_data(config)
+        # print(occupancy_data)
         if occupancy_data == 0:
             sleep(2)
             continue
         if int(occupancy_data['relay-function']) == 0:
-            print("relay function is off")
-            continue
+            # print("relay function is off")
+            if prev_in is None and prev_out is None:
+                
+                print("Starting for the first time")
+                prev_in = occupancy_data['in']
+                prev_out = occupancy_data['out']
+                continue
+            
+            in_diff = abs(prev_in - occupancy_data['in'])
+            out_diff = abs(prev_out - occupancy_data['out'])
+            
+            if in_diff > 50 or out_diff > 50:
+                prev_in = occupancy_data['in']
+                prev_out = occupancy_data['out']
+                continue
+            print(in_diff, out_diff)
+            
+            for _in in range(0, in_diff):
+                send_in_trigger(config['gpio'][0]['pin'], config)
+            for _out in range(0, out_diff):
+                send_in_trigger(config['gpio'][1]['pin'], config)
+            prev_in = occupancy_data['in']
+            prev_out = occupancy_data['out']
+            
         else:
             handle_gpio(config, occupancy_data)
         sleep(2)
